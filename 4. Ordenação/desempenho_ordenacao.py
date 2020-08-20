17984c70c44ce6bc879c0ab5ad860a47d278a08f
Diogo Viana <diogocampos@outlook.com>
2020-08-20 14:41:49 -0300

    Compara o desempenho dos métodos de ordenação

--- 4. Ordenação/desempenho_ordenacao.py
+++ 4. Ordenação/desempenho_ordenacao.py
@@ -6,31 +6,31 @@ import time
 class ContaTempos:
 
     def lista_aleatoria(self, n):
-        lista = [0 for x in range(n)]
-        for i in range(n):
-            lista[i] = random.randrange(1000)  # int entre 0 e 999
+        lista = [random.randrange(1000) for x in range(n)]
+        return lista
 
+    def lista_quase_ordenada(self, n):
+        lista = [x for x in range(n)]
+        lista[n//10] = -500
         return lista
 
     def compara(self, n):
-        lista1 = ct.lista_aleatoria(n)
-        lista2 = lista1[:]
+        lista1 = self.lista_aleatoria(n)
+        lista2, lista3 = lista1[:], lista1[:]
 
         o = ordenador.Ordenador()
 
         antes = time.time()
-        o.bolha(lista1)
+        o.bolha(lista3)
         depois = time.time()
-        print("Bolra demorou", depois - antes)
+        print("Bolha demorou", depois - antes)
 
         antes = time.time()
-        o.selecao_direta(lista2)
+        o.bolha_stop(lista2)
+        depois = time.time()
+        print("bolha stop demorou", depois - antes)
+
+        antes = time.time()
+        o.selecao_direta(lista1)
         depois = time.time()
         print("Selecao direta demorou", depois - antes)
-
-
-if __name__ == "__main__":
-
-    ct = ContaTempos()
-    tamanho_vetor = int(input("Quantidade de numeros a comparar: "))
-    ct.compara(tamanho_vetor)
