58d1af9a2c84b77d4e8fd869752f69b4947f5e60
Diogo Viana <diogocampos@outlook.com>
2020-08-20 14:41:19 -0300

    Adicionando metodo que cria lista quase ordenada

--- 4. Ordenação/ordenador.py
+++ 4. Ordenação/ordenador.py
@@ -26,6 +26,19 @@ class Ordenador:
                 if lista[j] > lista[j+1]:
                     lista[j], lista[j+1] = lista[j+1], lista[j]
 
+    def bolha_stop(self, lista):
+        fim = len(lista)
+
+        # range(inicia do fim do vetor, vá até a posição 0, passo de -1 em -1)
+        for i in range(fim-1, 0, -1):
+            trocou = False
+            for j in range(i):
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+                    trocou = True
+            if not trocou:
+                return
+
 
 if __name__ == "__main__":
     lista_sd = [4, 2, 7, 2, 6, 2, 9, 55, 1]
